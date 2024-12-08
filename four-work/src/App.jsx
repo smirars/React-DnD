3303231b28a65606a63858416373cb45083238d5
Arsonist <adsmi888@gmail.com>
2024-12-08 22:29:40 +0300

    change routing

--- four-work/src/App.jsx
+++ four-work/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import TodoPage from './pages/TodoPage';
 import DndPage from './pages/DndPage';
 function App() {
